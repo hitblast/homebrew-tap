53ce2a6abe26abaeea98e2bca4597eecc9fd2d61
Anindya Shiddhartha <shiddhartha@tuta.io>
2026-01-30 00:22:58 +0600

    chore: update cornelli to v1.3.0; remove jettison

--- Formula/cornelli.rb
+++ Formula/cornelli.rb
@@ -2,13 +2,13 @@
 
 # Write to your future self.
 class Cornelli < Formula
-  version '1.2.0'
+  version '1.3.0'
   desc 'Write to your future self.'
   homepage 'https://github.com/hitblast/cornelli'
 
   if Hardware::CPU.arm?
     url "https://github.com/hitblast/cornelli/releases/download/v#{version}/cornelli-macos-latest.tar.gz"
-    sha256 '76388e50ed89163fe2846174764ed3a21335d477cde61208c32990ffcdfead7b'
+    sha256 '6facff11f83ccff284b22567d2c5a0b539df7ebbf3ff449c2cc5e9f540b5e692'
   else
     odie 'cornelli is only available for ARM versions of macOS (Apple Silicon).'
   end

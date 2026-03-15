197cc650be4eab68e4dc835cc960eadaa2cc51f9
Anindya Shiddhartha <shiddhartha@tuta.io>
2026-03-16 01:15:43 +0600

    update cornelli to 1.4.1

--- Formula/cornelli.rb
+++ Formula/cornelli.rb
@@ -2,13 +2,13 @@
 
 # Write to your future self.
 class Cornelli < Formula
-  version '1.4.0'
+  version '1.4.1'
   desc 'Write to your future self.'
   homepage 'https://github.com/hitblast/cornelli'
 
   if Hardware::CPU.arm?
     url "https://github.com/hitblast/cornelli/releases/download/v#{version}/cornelli-macos-latest.tar.gz"
-    sha256 'ea1a1b259d79aa3037c5f3ebceb1b1cbf3dc9598b3f38b7c457dd91adaec5b86'
+    sha256 'aadb768bdbcea7e31d137b536e0ac5cc0e513a3a77a8178932bfa6fe80c81f8c'
   else
     odie 'cornelli is only available for ARM versions of macOS (Apple Silicon).'
   end

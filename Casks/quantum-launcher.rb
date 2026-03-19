59827d72af40e9f15e676f85083e289bba98acd7
Anindya Shiddhartha <shiddhartha@tuta.io>
2026-03-19 10:27:40 +0600

    chore: add quantum launcher

--- Casks/quantum-launcher.rb
+++ Casks/quantum-launcher.rb
@@ -4,7 +4,7 @@
 # DO NOT USE IN PRODUCTION
 
 cask 'quantum-launcher' do
-  version '0.5.0'
+  version '0.5.1'
 
   name 'QuantumLauncher'
   desc 'A minimalistic Minecraft launcher for Windows, macOS and Linux.'
@@ -13,17 +13,13 @@ cask 'quantum-launcher' do
   depends_on macos: '>= :catalina'
 
   on_arm do
-    url ""
-    sha256 ""
+    url "https://github.com/Mrmayman/quantumlauncher/releases/download/v0.5.1/quantum_launcher_macos_aarch64.dmg"
+    sha256 "5a9744d553e4871ee5a6300ec4660333e1fa35b9689b45c56540cb1ea219a596"
   end
 
   on_intel do
-    url ""
-    sha256 ""
-  end
-
-  postflight do
-    system_command '/usr/bin/xattr', args: ['-d', 'com.apple.quarantine', "#{appdir}/quantum-launcher.app"]
+    url "https://github.com/Mrmayman/quantumlauncher/releases/download/v0.5.1/quantum_launcher_macos_x86_64.dmg"
+    sha256 "6a0e9fcaa5cd92080689a9c77d11475272ce2d6e78c0ddc0d8b3fc39a6dc8b9d"
   end
 
   app 'quantum-launcher.app'

53a2707742f6c3a46bb40eac4ef96774461650e3
Anindya Shiddhartha <shiddhartha@tuta.io>
2026-04-06 22:53:22 +0600

    update tap

--- Casks/quantum-launcher.rb
+++ Casks/quantum-launcher.rb
@@ -2,25 +2,18 @@ cask "quantum-launcher" do
   version "0.5.1"
 
   on_arm do
-    sha256 "5a9744d553e4871ee5a6300ec4660333e1fa35b9689b45c56540cb1ea219a596"
-
     url "https://github.com/Mrmayman/quantumlauncher/releases/download/v#{version}/quantum_launcher_macos_aarch64.dmg"
   end
   on_intel do
-    sha256 "6a0e9fcaa5cd92080689a9c77d11475272ce2d6e78c0ddc0d8b3fc39a6dc8b9d"
-
     url "https://github.com/Mrmayman/quantumlauncher/releases/download/v#{version}/quantum_launcher_macos_x86_64.dmg"
   end
 
   name "QuantumLauncher"
-  desc "Minimalistic Minecraft launcher"
+  desc "A simple, powerful Minecraft launcher"
   homepage "https://github.com/Mrmayman/quantumlauncher"
+  sha256 :no_check
 
   depends_on macos: ">= :big_sur"
 
   app "quantum-launcher.app"
-
-  caveats do
-    signed "false"
-  end
 end

ab96513131b5d480236f540207b2b26ffe0f32ac
Anindya Shiddhartha <shiddhartha@tuta.io>
2026-04-06 22:58:05 +0600

    re-include things

--- Casks/quantum-launcher.rb
+++ Casks/quantum-launcher.rb
@@ -1,12 +1,13 @@
 cask "quantum-launcher" do
   version "0.5.1"
-  sha256 :no_check
 
   on_arm do
     url "https://github.com/Mrmayman/quantumlauncher/releases/download/v#{version}/quantum_launcher_macos_aarch64.dmg"
+    sha256 "5a9744d553e4871ee5a6300ec4660333e1fa35b9689b45c56540cb1ea219a596"
   end
   on_intel do
     url "https://github.com/Mrmayman/quantumlauncher/releases/download/v#{version}/quantum_launcher_macos_x86_64.dmg"
+    sha256 "6a0e9fcaa5cd92080689a9c77d11475272ce2d6e78c0ddc0d8b3fc39a6dc8b9d"
   end
 
   name "QuantumLauncher"
@@ -14,6 +15,10 @@ cask "quantum-launcher" do
   homepage "https://github.com/Mrmayman/quantumlauncher"
 
   depends_on macos: ">= :big_sur"
+
+  postflight do
+    system_command "/usr/bin/xattr", args: ["-r", "-c", "#{appdir}/quantum-launcher.app"]
+  end
 
   app "quantum-launcher.app"
 end

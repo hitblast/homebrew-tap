7ed35f8f687d9526428fba80b9beeac237b6aac2
Anindya Shiddhartha <shiddhartha@tuta.io>
2026-07-12 10:52:59 +0600

    update preconnect to **7036

--- Casks/preconnect.rb
+++ Casks/preconnect.rb
@@ -1,10 +1,10 @@
 cask "preconnect" do
-  version "1.6.8+202607027"
+  version "1.6.8+202607036"
 
   on_arm do
     escaped_version = version.to_s.gsub("+", "%2B")
     url "https://github.com/sabbirba/preconnect/releases/download/v#{escaped_version}/PreConnect-macos-release-#{escaped_version}.dmg"
-    sha256 "cbfd06b7fb862c9488a8e74090799a98f1e90ca56ec6c3f88a3b35f0ae499217"
+    sha256 "fe9238c5711c1d6c65c1836ab66df63b8e326f4b04590b43638376cfb70d348f"
   end
 
   name "PreConnect"

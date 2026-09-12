92ac84f630b787014e4b4703a10bda60b3cf9991
Anindya Shiddhartha <shiddhartha@tuta.io>
2026-09-12 14:33:50 +0600

    update preconnect

--- Casks/preconnect.rb
+++ Casks/preconnect.rb
@@ -3,7 +3,9 @@ cask "preconnect" do
 
   on_arm do
     escaped_version = version.to_s.gsub("+", "%2B")
+
     url "https://github.com/sabbirba/preconnect/releases/download/v#{escaped_version}/PreConnect-macos-release-#{escaped_version}.pkg"
+
     sha256 "a4c2d8671ae82b6756e07e63a5318ffdb5fc49ac65b2ed39cbb265f59650c943"
   end
 
@@ -13,8 +15,12 @@ cask "preconnect" do
 
   depends_on macos: :big_sur
 
+  pkg "PreConnect-macos-release-#{escaped_version}.pkg"
+
   postflight do
-    system_command "/usr/bin/xattr", args: ["-r", "-c", "#{appdir}/PreConnect.app"], must_succeed: false
+    system_command "/usr/bin/xattr",
+                   args: ["-r", "-c", "#{appdir}/PreConnect.app"],
+                   must_succeed: false
   end
 
   app "PreConnect.app"

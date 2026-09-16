dc9a507cf1a9b9eadd49ef35602aab6ac6de5b72
Anindya Shiddhartha <shiddhartha@tuta.io>
2026-09-16 12:20:40 +0600

    updated preconnect cask to use `depends_on arch:`

--- Casks/preconnect.rb
+++ Casks/preconnect.rb
@@ -1,16 +1,16 @@
 cask "preconnect" do
   version "2.0.9+202609013"
+  escaped_version = version.to_s.gsub("+", "%2B")
+
+  url "https://github.com/sabbirba/preconnect/releases/download/v#{escaped_version}/PreConnect-macos-release-#{escaped_version}.pkg"
+  sha256 "a4c2d8671ae82b6756e07e63a5318ffdb5fc49ac65b2ed39cbb265f59650c943"
 
   name "PreConnect"
   desc "Fast, Calm Academic Companion App. An initiative run by BRAC University students."
   homepage "https://github.com/sabbirba/preconnect"
 
   depends_on macos: :big_sur
-
-  on_arm do
-    url "https://github.com/sabbirba/preconnect/releases/download/v#{version}/PreConnect-macos-release-#{version}.pkg"
-    sha256 "a4c2d8671ae82b6756e07e63a5318ffdb5fc49ac65b2ed39cbb265f59650c943"
-  end
+  depends_on arch: :arm64
 
   pkg "PreConnect-macos-release-#{version}.pkg"
   app "PreConnect.app"

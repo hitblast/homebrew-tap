0bed58c0217daf19e9e588b56720315fe16b7bd6
Anindya Shiddhartha <shiddhartha@tuta.io>
2026-09-22 21:43:52 +0600

    update preconnect to latest version

--- Casks/preconnect.rb
+++ Casks/preconnect.rb
@@ -1,9 +1,9 @@
 cask "preconnect" do
-  version "2.0.9+202609013"
+  version "2.0.9+202609020"
   escaped_version = version.to_s.gsub("+", "%2B")
 
   url "https://github.com/sabbirba/preconnect/releases/download/v#{escaped_version}/PreConnect-macos-release-#{escaped_version}.pkg"
-  sha256 "a4c2d8671ae82b6756e07e63a5318ffdb5fc49ac65b2ed39cbb265f59650c943"
+  sha256 "194eccb8933f81ca670fb1a54d8aa9b640d2e8f98ede392411cd9ef8dd380a0c"
 
   name "PreConnect"
   desc "Fast, Calm Academic Companion App. An initiative run by BRAC University students."

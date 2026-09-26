d6fcc29162f5a9338c926cc7ae0ecf9c23389c39
Anindya Shiddhartha <shiddhartha@tuta.io>
2026-09-26 13:43:34 +0600

    update trimsec to v4.2.0

--- Formula/trimsec.rb
+++ Formula/trimsec.rb
@@ -2,13 +2,13 @@
 
 # Calculate saved time on videos with multipliers.
 class Trimsec < Formula
-  version '4.1.0'
+  version '4.2.0'
   desc 'Plan your content intake.'
   homepage 'https://github.com/hitblast/trimsec'
 
   if Hardware::CPU.arm?
     url "https://github.com/hitblast/trimsec/releases/download/v#{version}/trimsec-macos-latest.tar.gz"
-    sha256 'a1c57356d36d7595d5eccb1e52d1d82fbf46e8cfd60d0d0e2b18e51cc37de39c'
+    sha256 '2e659fd7811cf06d9303735875f0dd495527bcb9f64cea05322f47250da5adc2'
   else
     odie 'trimsec is only available for ARM versions of macOS (Apple Silicon).'
   end
